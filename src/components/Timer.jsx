62eed512590382a0d82319536f40141ec9d249d3
Vladislav Hilko <uladzislau.hilko@gmail.com>
2024-02-06 15:05:47 +0100

    check basic logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,20 +1,53 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, Stack } from '@chakra-ui/react'
 
 const Timer = () => {
+
+  const [seconds, setSeconds] = useState(0);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    timerRef.current = setInterval(resumeCountdown, 1000);
+
+    return () => clearInterval(timerRef.current);
+  }, []);
+
+  const resumeCountdown= () => {
+    setSeconds(prevValue => prevValue + 1);
+  }
+
+  const pauseTimer = () => {
+    clearInterval(timerRef.current)
+  }
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current)
+    setSeconds(0);
+  }
+
+  const resumeTimer = () => {
+    clearInterval(timerRef.current);
+    resumeCountdown();
+    timerRef.current = setInterval(resumeCountdown, 1000);
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-600 text-white">
+    <div className="flex flex-col items-center justify-center h-screen bg-emerald-300 text-white">
+      <div className="text-9xl font-mono mb-8">
+        10:{seconds}
+      </div>
       <div className="mb-8">
         <Stack direction='row' spacing={4} align='center'>
           <Button colorScheme='teal' variant='outline'>
             START
           </Button>
-          <Button colorScheme='teal' variant='outline'>
-            STOP
+          <Button colorScheme='teal' variant='outline' onClick={resetTimer}>
+            RESET
           </Button>
-          <Button colorScheme='teal' variant='outline'>
+          <Button colorScheme='teal' variant='outline' onClick={resumeTimer}>
             RESUME
           </Button>
-          <Button colorScheme='teal' variant='outline'>
+          <Button colorScheme='teal' variant='outline' onClick={pauseTimer}>
             PAUSE
           </Button>
         </Stack>
