30963b1bfc3e8aee46cbc64568ab94c5aa283615
Vladislav Hilko <uladzislau.hilko@gmail.com>
2024-02-06 13:19:50 +0100

    Add TaiwindCSS initialization

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,21 +2,23 @@ import { Button, Stack } from '@chakra-ui/react'
 
 const Timer = () => {
   return (
-    <div>
-      <Stack direction='row' spacing={4} align='center'>
-        <Button colorScheme='teal' variant='outline'>
-          START
-        </Button>
-        <Button colorScheme='teal' variant='outline'>
-          STOP
-        </Button>
-        <Button colorScheme='teal' variant='outline'>
-          RESUME
-        </Button>
-        <Button colorScheme='teal' variant='outline'>
-          PAUSE
-        </Button>
-      </Stack>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-600 text-white">
+      <div className="mb-8">
+        <Stack direction='row' spacing={4} align='center'>
+          <Button colorScheme='teal' variant='outline'>
+            START
+          </Button>
+          <Button colorScheme='teal' variant='outline'>
+            STOP
+          </Button>
+          <Button colorScheme='teal' variant='outline'>
+            RESUME
+          </Button>
+          <Button colorScheme='teal' variant='outline'>
+            PAUSE
+          </Button>
+        </Stack>
+      </div>
     </div>
   )
 }
