ae0da24a43bcb74a70abaa69780a830e5a7f35fb
Vladislav Hilko <uladzislau.hilko@gmail.com>
2024-02-06 12:58:36 +0100

    Add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Timer from './components/Timer';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Timer></Timer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
